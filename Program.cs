c1058e140c11ce868d57e90f33d325db529373b4
Bezverkhniy Andrey <banbezv@mail.ru>
2021-10-27 23:09:24 +0300

    almost initializeSimplex

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
 ﻿using System;
 using System.IO;
+class Utils{
+    public static bool consists(int[] arr,int element){
+        for(int i=0;i<arr.Length;i++){
+            if(arr[i]==element){
+                return true;
+            }
+        }
+        return false;
+    }
+}
 
 class Standartic{
     public int n;
@@ -60,6 +70,29 @@ class Standartic{
             Console.WriteLine(" <= {0}",b[i]);
         }
     }
+    public Standartic getAUX(){
+        Standartic aux = new Standartic();
+        aux.n=n+1;
+        aux.m=m;
+        aux.b = new double[m];
+        for(int i=0;i<m;i++){
+            aux.b[i]=b[i];
+        }
+        aux.c = new double[n+1];
+        for(int i=0;i<n;i++){
+            aux.c[i]=0;
+        }
+        aux.c[n]=-1;
+        aux.A=new double[m][];
+        for(int i=0;i<m;i++){
+            aux.A[i]=new double[n+1];
+            for(int j=0;j<n;j++){
+                aux.A[i][j]=A[i][j];
+            }
+            aux.A[i][n]=-1;
+        }
+        return aux;
+    }
 }
 
 class Canonic{
@@ -101,13 +134,39 @@ class Canonic{
         }
 
     }
-    bool consists(int[] arr,int element){
-        for(int i=0;i<arr.Length;i++){
-            if(arr[i]==element){
-                return true;
+    public Canonic(Standartic standartic){
+        m = standartic.m;
+        n = standartic.n + standartic.m;
+        v = 0;
+        N = new int[standartic.n];
+        for (int i=0;i<standartic.n;i++) {
+            N[i]=i;
+        }
+        B = new int[standartic.m];
+        for(int i=0;i<standartic.m;i++) {
+            B[i]=standartic.n+i;
+        }
+        A = new double[standartic.m][];
+        for (int i=0;i<standartic.m;i++) {
+            A[i] = new double[standartic.n+standartic.m];
+            for(int j=0;j<standartic.n;j++){
+                A[i][j]=standartic.A[i][j];
+            }
+            for(int j=standartic.n;j<standartic.n+standartic.m;j++){
+                A[i][j]=0;
             }
         }
-        return false;
+        c = new double[standartic.n+standartic.m];
+        for(int i=0;i<standartic.n;i++){
+            c[i]=standartic.c[i];
+        }
+        for(int i=standartic.n;i<standartic.n+standartic.m;i++){
+            c[i]=0;
+        }
+        b = new double[standartic.m];
+        for(int i=0;i<standartic.m;i++){
+            b[i]=standartic.b[i];
+        }
     }
     public void LoadFromFile(string path){
         StreamReader R;
@@ -138,7 +197,7 @@ class Canonic{
         N = new int [n];
         int index=0;
         for(int i=0;index<n;i++){
-            if(!consists(B,i)){
+            if(!Utils.consists(B,i)){
                 N[index]=i;
                 index++;
             }
@@ -163,13 +222,13 @@ class Canonic{
         Console.WriteLine("Canonic");
         Console.Write("z={0}",v);
         for(int i=0;i<n;i++){
-            Console.Write(" + {0}*x{1}",c[i],i);
+            Console.Write(" + ({0})*x{1}",c[i],i);
         }
         Console.WriteLine();
         for(int i=0;i<m;i++){
             Console.Write("x{0} = {1}",B[i],b[i]);
             for(int j=0;j<n;j++){
-                Console.Write(" - {0}*x{1}",A[i][j],j);
+                Console.Write(" - ({0})*x{1}",A[i][j],j);
             }
             Console.WriteLine();
         }
@@ -218,6 +277,14 @@ class Canonic{
             result.c[i]=c[i]-c[e]*result.A[equality][i];
         }
         return result;
+    }
+    public double getXN(int index){
+        for(int i=0;i<m;i++){
+            if(B[i]==index){
+                return b[i];
+            }
+        }
+        return 0;
     }
 }
 
@@ -278,6 +345,67 @@ class Program
         }
         Console.WriteLine();
     }
+    static Canonic Initialize(Standartic standartic){
+        int index_min_b=0;
+        for(int i=0;i<standartic.m;i++){
+            if(standartic.b[index_min_b]>standartic.b[i]){
+                index_min_b=i;
+            }
+        }
+        Console.WriteLine("index_min_b={0}",index_min_b);
+        if(standartic.b[index_min_b]>=0){
+            Console.WriteLine("Start sol ok");
+            return new Canonic(standartic);
+        }
+        Standartic aux = standartic.getAUX();
+        Canonic c_aux = new Canonic(aux);
+        c_aux = c_aux.Pivot(aux.n+index_min_b,aux.n);
+
+        double[] delta=new double[c_aux.m];
+        for(;;){
+            //c_aux.Print();
+            int e=-1;
+            for(int i=0;i<c_aux.n;i++){
+                if(c_aux.c[i]>0){
+                    e=i;
+                    break;
+                }
+            }
+            if(e==-1){
+                break;
+            }
+            for(int equality=0;equality<c_aux.B.Length;equality++){
+                if(c_aux.A[equality][e]>0){
+                    delta[equality]=c_aux.b[equality]/c_aux.A[equality][e];
+                } else {
+                    delta[equality]=double.PositiveInfinity;
+                }
+            }
+            /*Console.Write("delta: ");
+            for(int equality=0;equality<delta.Length;equality++){
+                Console.Write("{0}/{1}={2} ",c_aux.b[equality],
+                    c_aux.A[equality][e],delta[equality]);
+            }
+            Console.WriteLine();//*/
+            int l_equality=0;
+            for(int i=1;i<c_aux.B.Length;i++){
+                if(delta[i]<delta[l_equality]){
+                    l_equality=i;
+                }
+            }
+            int l=c_aux.B[l_equality];
+            //Console.WriteLine("e={0} l={1}",e,l);
+            c_aux = c_aux.Pivot(l,e);
+        }
+        if(c_aux.getXN(aux.n)!=0){
+            Console.WriteLine("Задача неразрешима");
+        } else {
+            if(Utils.consists(c_aux.B,aux.n)){
+                
+            }
+        }
+        return new Canonic();
+    }
     static void Main(string[] args)
     {
         //Canonic canonic = new Canonic();
@@ -286,5 +414,10 @@ class Program
         Standartic standartic = new Standartic();
         standartic.LoadFromFile("stan1.txt");
         standartic.Print();
+        Standartic aux;
+        aux = standartic.getAUX();
+        aux.Print();
+        //Canonic canonic = new Canonic();
+        //canonic = Initialize(standartic);
     }
 }
