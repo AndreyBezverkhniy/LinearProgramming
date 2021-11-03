3c4a4c14349a16982ecbaf748263aa9555c1728d
Bezverkhniy Andrey <banbezv@mail.ru>
2021-11-03 21:17:27 +0300

    refactoring in progress

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 class Utils{
-    public static bool consists(int[] arr,int element){
+    public static bool DoesArrayConsistsElement(int[] arr,int element){
         for(int i=0;i<arr.Length;i++){
             if(arr[i]==element){
                 return true;
@@ -9,46 +9,54 @@ class Utils{
         }
         return false;
     }
-}
-
-class Standartic{
-    public int n;
-    public int m;
-    public double[][] A;
-    public double[] b;
-    public double[] c;
-    public Standartic(){}
-    public void LoadFromFile(string path){
-        StreamReader R;
-        R = new StreamReader(path);
+    public static int ReadIntFromFile(StreamReader file){
+        return int.Parse(file.ReadLine());
+    }
+    public static double[] ReadSequenceFromFile(StreamReader file,int size){
         string str;
         string[] strArr;
-        // n
-        n = int.Parse(R.ReadLine());
-        // m
-        m = int.Parse(R.ReadLine());
-        // c
-        str = R.ReadLine();
+        double[] array = new double[size];
+        str = file.ReadLine();
         strArr = str.Split(" ");
-        c = new double[n];
-        for(int i=0;i<n;i++){
-            c[i]=double.Parse(strArr[i]);
+        array = new double[size];
+        for(int i=0;i<size;i++){
+            array[i]=double.Parse(strArr[i]);
         }
-        // A & b
-        A = new double[m][];
-        for(int i=0;i<m;i++){
-            A[i]=new double[n];
-        }
-        b = new double[m];
-        for(int i=0;i<m;i++){
-            str = R.ReadLine();
+        return array;
+    }
+    public static void ReadSystemFromFile(StreamReader file,
+    out double[][] A,out double[] b,int lines,int variableCount){
+        string str;
+        string[] strArr;
+        A = new double[lines][];
+        b = new double[lines];
+        for(int i=0;i<lines;i++){
+            str = file.ReadLine();
             strArr = str.Split(" ");
-            b[i] = double.Parse(strArr[n]);
-            for(int j=0;j<n;j++){
+            A[i]=new double[variableCount];
+            b[i] = double.Parse(strArr[variableCount]);
+            for(int j=0;j<variableCount;j++){
                 A[i][j]=double.Parse(strArr[j]);
             }
         }
-        R.Close();
+    }
+}
+
+class Standartic{
+    public int n; // кол-во оригинальных переменных
+    public int m; // кол-во неравенств-ограничений
+    public double[][] A; // матрица системы неравенств
+    public double[] b; // свободные члены (правая часть неравенств)
+    public double[] c; // вектор коэффициентов в целевой функции
+    public Standartic(){} // по умолчанию ничего не существует
+    public void LoadFromFile(string path){
+        StreamReader file;
+        file = new StreamReader(path);
+        n = Utils.ReadIntFromFile(file);
+        m = Utils.ReadIntFromFile(file);
+        c = Utils.ReadSequenceFromFile(file,n);
+        Utils.ReadSystemFromFile(file,out A,out b,m,n);
+        file.Close();
     }
     public void Print(){
         Console.WriteLine("Standartic");
@@ -93,26 +101,33 @@ class Standartic{
         }
         return aux;
     }
+    public int getMinBIndex(){
+        int index_min_b=0;
+        for(int i=0;i<m;i++){
+            if(b[index_min_b]>b[i]){
+                index_min_b=i;
+            }
+        }
+        return index_min_b;
+    }
+    public int getAdditionalVariableIndexAUX(){
+        return n;
+    }
 }
 
 class Canonic{
-    public int n;
-    public int m;
-    public int[] N;
-    public int[] B;
-    public double[][] A;
-    public double[] b;
-    public double[] c;
-    public double v;
-    public Canonic(){}
+    public int n; // кол-во оригинальных и дополнительных переменных
+    public int m; // кол-во неравенств-ограничений
+    public int[] B; // множество базисных переменных
+    public double[][] A; // матрица новой системы неравенств
+    public double[] b; // свободные члены (правая часть неравенств)
+    public double[] c; // новый вектор коэффициентов в целевой функции
+    public double v; // значение целевой функции при 0-х небазисных переменных
+    public Canonic(){} // по умолчанию ничего не существует
     public Canonic(Canonic copy){
         n=copy.n;
         m=copy.m;
         v=copy.v;
-        N=new int[copy.N.Length];
-        for(int i=0;i<N.Length;i++){
-            N[i]=copy.N[i];
-        }
         B=new int[copy.B.Length];
         for(int i=0;i<B.Length;i++){
             B[i]=copy.B[i];
@@ -135,18 +150,14 @@ class Canonic{
 
     }
     public Canonic(Standartic standartic){
-        m = standartic.m;
-        n = standartic.n + standartic.m;
+        m = standartic.m; // кол-во неравенств тоже
+        n = standartic.n + standartic.m; // оригинальные и дополнительные
         v = 0;
-        N = new int[standartic.n];
-        for (int i=0;i<standartic.n;i++) {
-            N[i]=i;
-        }
-        B = new int[standartic.m];
+        B = new int[standartic.m]; // сначала: дополнительные переменные
         for(int i=0;i<standartic.m;i++) {
             B[i]=standartic.n+i;
         }
-        A = new double[standartic.m][];
+        A = new double[standartic.m][]; // расширение матрицы стандартной формы
         for (int i=0;i<standartic.m;i++) {
             A[i] = new double[standartic.n+standartic.m];
             for(int j=0;j<standartic.n;j++){
@@ -156,6 +167,7 @@ class Canonic{
                 A[i][j]=0;
             }
         }
+         // добавление 0-х коэффициентов при дополнительных переменных
         c = new double[standartic.n+standartic.m];
         for(int i=0;i<standartic.n;i++){
             c[i]=standartic.c[i];
@@ -163,60 +175,10 @@ class Canonic{
         for(int i=standartic.n;i<standartic.n+standartic.m;i++){
             c[i]=0;
         }
-        b = new double[standartic.m];
+        b = new double[standartic.m]; // вектор свободных членов такой же
         for(int i=0;i<standartic.m;i++){
             b[i]=standartic.b[i];
         }
-    }
-    public void LoadFromFile(string path){
-        StreamReader R;
-        R = new StreamReader(path);
-        string str;
-        string[] strArr;
-        // n
-        n = int.Parse(R.ReadLine());
-        // m
-        m = int.Parse(R.ReadLine());
-        // v
-        v = double.Parse(R.ReadLine());
-        // c
-        str = R.ReadLine();
-        strArr = str.Split(" ");
-        c = new double[n];
-        for(int i=0;i<n;i++){
-            c[i]=double.Parse(strArr[i]);
-        }
-        // B
-        str = R.ReadLine();
-        strArr = str.Split(" ");
-        B = new int[m];
-        for(int i=0;i<m;i++){
-            B[i]=int.Parse(strArr[i]);
-        }
-        // N
-        N = new int [n];
-        int index=0;
-        for(int i=0;index<n;i++){
-            if(!Utils.consists(B,i)){
-                N[index]=i;
-                index++;
-            }
-        }
-        // A & b
-        A = new double[m][];
-        for(int i=0;i<m;i++){
-            A[i]=new double[n];
-        }
-        b = new double[m];
-        for(int i=0;i<m;i++){
-            str = R.ReadLine();
-            strArr = str.Split(" ");
-            b[i] = double.Parse(strArr[0]);
-            for(int j=0;j<n;j++){
-                A[i][j]=double.Parse(strArr[1+j]);
-            }
-        }
-        R.Close();
     }
     public void Print(){
         Console.WriteLine("Canonic");
@@ -233,7 +195,7 @@ class Canonic{
             Console.WriteLine();
         }
     }
-    public int equalityByBasic(int vIndex){
+    private int equalityByBasic(int vIndex){
         for(int i=0;i<m;i++){
             if(B[i]==vIndex){
                 return i;
@@ -241,44 +203,63 @@ class Canonic{
         }
         return -1;
     }
-    public Canonic Pivot(int l,int e){
-        Canonic result=new Canonic(this);
-        int equality = equalityByBasic(l);
-        result.B[equality]=e;
-        result.c[e]=0;
+    private void exchangeVariablesInEquation(int leaving,int entering,
+    ref double[] coefficients,ref double freeMember){
+        if(coefficients[entering]==0){
+            throw new Exception();
+        }
+        freeMember/=coefficients[entering];
+        coefficients[leaving]=1.0/coefficients[entering];
+        for(int i=0;i<n;i++){
+            if(i==leaving || i==entering){
+                continue;
+            }
+            coefficients[i]/=coefficients[entering];
+        }
+        coefficients[entering]=0;
+    }
+    private void substituteVariableInEquation(int variable,
+    double[] vCoefficients,double vFreeMember,ref double[] coefficients,
+    ref double freeMember){
+        freeMember+=vFreeMember*coefficients[variable];
+        for(int i=0;i<n;i++){
+            if(i==variable){
+                continue;
+            }
+            coefficients[i]=coefficients[i]+coefficients[variable]*vCoefficients[i];
+        }
+        coefficients[variable]=0;
+    }
+    private void PivotRecalculateRestrictions(int leaving,int entering){
+        int equality = equalityByBasic(leaving);
+        B[equality]=entering;
+        exchangeVariablesInEquation(leaving,entering,ref A[equality],
+        ref b[equality]);
         for(int i=0;i<m;i++){
-            result.A[i][e]=0;
-        }
-        result.b[equality]=b[equality]/A[equality][e];
-        result.A[equality][l]=1/A[equality][e];
-        for(int i=0;i<n;i++){
-            if(i==e||i==l){
+            if(i==equality){
                 continue;
             }
-            result.A[equality][i]=A[equality][i]/A[equality][e];
+            substituteVariableInEquation(entering,A[equality],b[equality],
+            ref A[i],ref b[i]);
         }
-        for(int k=0;k<m;k++){
-            if(k==equality){
-                continue;
-            }
-            result.b[k]=b[k]-A[k][e]*result.b[equality];
-            for(int i=0;i<n;i++){
-                if(i==e){
-                    continue;
-                }
-                result.A[k][i]=A[k][i]-A[k][e]*result.A[equality][i];
-            }
-        }
-        result.v=v+c[e]*result.b[equality];
-        for(int i=0;i<n;i++){
-            if(i==e){
-                continue;
-            }
-            result.c[i]=c[i]-c[e]*result.A[equality][i];
-        }
+    }
+    private void PivotRecalculateFunction(int leaving,int entering){
+        int equality = equalityByBasic(leaving);
+        substituteVariableInEquation(entering,A[equality],b[equality],ref c,
+        ref v);
+    }
+    private void PivotRedivideBasics(int leaving,int entering){
+        int equality = equalityByBasic(leaving);
+        B[equality]=entering;
+    }
+    public Canonic Pivot(int leaving,int entering){
+        Canonic result=new Canonic(this);
+        PivotRecalculateRestrictions(leaving,entering);
+        PivotRecalculateFunction(leaving,entering);
+        PivotRedivideBasics(leaving,entering);
         return result;
     }
-    public double getXN(int index){
+    public double getVariableXNValue(int index){
         for(int i=0;i<m;i++){
             if(B[i]==index){
                 return b[i];
@@ -288,13 +269,37 @@ class Canonic{
     }
 }
 
-class Program
-{
-    static void Simplex(Canonic canonic){
-        ///// стартовое допустимое решение
-        double[] delta=new double[canonic.m];
+class SimplexAnswer{
+    public Canonic canonic;
+    public string type;
+    public SimplexAnswer(Canonic _canonic){
+        canonic = _canonic;
+        type = "solved";
+    }
+    public SimplexAnswer(string _type){
+        canonic = new Canonic();
+        type = _type;
+    }
+    public void PrintAnswer(){
+        for(int i=0;i<canonic.n;i++){
+            Console.Write("x{0}={1} ",i,canonic.getVariableXNValue(i));
+        }
+        Console.WriteLine();
+    }
+}
+
+class Program{
+    static SimplexAnswer Simplex(Standartic standartic){
+        // поиск начального допустимого решения
+        SimplexAnswer startingSolution=Initialize(standartic);
+        if(startingSolution.type!="solved"){
+            return startingSolution;
+        }
+        // поиск оптимального решения
+        Canonic canonic=startingSolution.canonic;
         for(;;){
             canonic.Print();
+            // поиск вводимой переменной
             int e=-1;
             for(int i=0;i<canonic.n;i++){
                 if(canonic.c[i]>0){
@@ -302,9 +307,12 @@ class Program
                     break;
                 }
             }
+            // если все коэф-ты в z <=0 достигнуто оптимальное решение
             if(e==-1){
                 break;
             }
+            // поиск максимального приращения вводимой переменной
+            double[] delta=new double[canonic.m];
             for(int equality=0;equality<canonic.B.Length;equality++){
                 if(canonic.A[equality][e]>0){
                     delta[equality]=canonic.b[equality]/canonic.A[equality][e];
@@ -312,54 +320,41 @@ class Program
                     delta[equality]=double.PositiveInfinity;
                 }
             }
-            Console.Write("delta: ");
-            for(int equality=0;equality<delta.Length;equality++){
-                Console.Write("{0}/{1}={2} ",canonic.b[equality],
-                    canonic.A[equality][e],delta[equality]);
-            }
-            Console.WriteLine();
+            // поиск выводимой переменной
             int l_equality=0;
             for(int i=1;i<canonic.B.Length;i++){
                 if(delta[i]<delta[l_equality]){
                     l_equality=i;
                 }
             }
+            // отслеживание случая неограниченности задачи
             if(double.IsPositiveInfinity(delta[l_equality])){
-                Console.WriteLine("Задача неограничена");
-                return;
-            } else {
-                int l=canonic.B[l_equality];
-                Console.WriteLine("e={0} l={1}",e,l);
-                canonic = canonic.Pivot(l,e);
+                return new SimplexAnswer("Задача неограничена");
             }
+            // переход от старой вершины симплекса к новой
+            int l=canonic.B[l_equality];
+            canonic = canonic.Pivot(l,e);
         }
-        double[] result=new double[canonic.n];
-        for(int i=0;i<canonic.n;i++){
-            result[i]=0;
-        }
-        for(int equality=0;equality<canonic.m;equality++){
-            result[canonic.B[equality]]=canonic.b[equality];
-        }
-        for(int i=0;i<result.Length;i++){
-            Console.Write("x{0}={1} ",i,result[i]);
-        }
-        Console.WriteLine();
+        return new SimplexAnswer(canonic);
     }
-    static Canonic Initialize(Standartic standartic){
-        int index_min_b=0;
-        for(int i=0;i<standartic.m;i++){
-            if(standartic.b[index_min_b]>standartic.b[i]){
-                index_min_b=i;
-            }
-        }
+    static SimplexAnswer Initialize(Standartic standartic){
+        int index_min_b=standartic.getMinBIndex();
         Console.WriteLine("index_min_b={0}",index_min_b);
+        // пустимо ли начальное решение
         if(standartic.b[index_min_b]>=0){
             Console.WriteLine("Start sol ok");
             return new Canonic(standartic);
         }
+        
+        // переходим к вспомогательной задаче
         Standartic aux = standartic.getAUX();
         Canonic c_aux = new Canonic(aux);
-        c_aux = c_aux.Pivot(aux.n+index_min_b,aux.n);
+
+        // выводимой переменной соответствует макс по модулю отрицательное b
+        int leaving=c_aux.B[index_min_b];
+        int entering=aux.getAdditionalVariableIndexAUX();
+        c_aux = c_aux.Pivot(leaving,entering);
+
 
         double[] delta=new double[c_aux.m];
         for(;;){
@@ -397,10 +392,10 @@ class Program
             //Console.WriteLine("e={0} l={1}",e,l);
             c_aux = c_aux.Pivot(l,e);
         }
-        if(c_aux.getXN(aux.n)!=0){
+        if(c_aux.getVariableXNValue(aux.n)!=0){
             Console.WriteLine("Задача неразрешима");
         } else {
-            if(Utils.consists(c_aux.B,aux.n)){
+            if(Utils.DoesArrayConsistsElement(c_aux.B,aux.n)){
                 
             }
         }
